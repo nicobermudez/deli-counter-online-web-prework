8e008ae5b240b2a729f2f634f19d73a5e3e4b015
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-21 19:42:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,13 @@ katz_deli = []
 def line(array)
   if array = [] || array = nil 
     puts "The line is currently empty."
+  
   end
   
 end
 
 def take_a_number(array, name)
+  array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
